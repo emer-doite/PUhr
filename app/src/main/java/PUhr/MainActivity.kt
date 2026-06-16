e13c95bd04d375a52ba1dc80823d8541451ec721
Emerson Gonzalez <emersong@users.noreply.github.com>
2026-06-16 15:37:26 -0400

    chore(nav): remove unused animation imports from MainActivity

--- app/src/main/java/PUhr/MainActivity.kt
+++ app/src/main/java/PUhr/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.scaleIn
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
